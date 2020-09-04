8ca63974e3f36e4ceb8bfa0074c5546d3007d5b8
Karlos Chita <kchita@tacitknowledge.com>
2020-09-04 13:37:59 -0500

    Add Spring boot base code

---
+++ src/main/java/sv/com/oci/google/authentication/GoogleAuthenticationApplication.java
@@ -0,0 +1,13 @@
+package sv.com.oci.google.authentication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GoogleAuthenticationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GoogleAuthenticationApplication.class, args);
+	}
+
+}
